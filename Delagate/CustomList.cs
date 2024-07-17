3314733e5ad8dc95eddd971826cf2dc5be58e4d4
MusaCodeBB <musachm@code.edu.az>
2024-07-17 17:56:20 +0400

    2 commit

--- Delagate/CustomList.cs
+++ Delagate/CustomList.cs
@@ -1,12 +1,67 @@
-﻿using System;
+﻿using Delagate.Exceptions;
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Delagate
+namespace Delagate;
+
+public class CustomList<T> : IEnumerable<T>
 {
-    internal class CustomList
+    private List<T> values;
+    public CustomList()
     {
+        values = new List<T>();
+    }
+    public void Add(T item)
+    {
+        values.Add(item);
+    }
+    public T Find(Predicate<T> item)
+    {
+        if (item == null)
+        {
+            throw new ArgumentNullException(nameof(item));
+        }
+
+        foreach (T value in values)
+        {
+            if (item(value))
+            {
+                return value;
+            }
+        }
+
+        return default(T);
+    }
+    public List<T> FindAll(Predicate<T> item)
+    {
+        if (item == null)
+        {
+            throw new NotFoundException(nameof(item));
+        }
+
+        List<T> result = new List<T>();
+
+        foreach (T value in values)
+        {
+            if (item(value))
+            {
+                result.Add(value);
+            }
+        }
+
+        return result;
+    }
+    public IEnumerator<T> GetEnumerator()
+    {
+        throw new NotImplementedException();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        throw new NotImplementedException();
     }
 }
--- Delagate/Program.cs
+++ Delagate/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Delagate;
+
+CustomList<byte> list = new CustomList<byte>();
+
+list.Add(1);
+list.Add(2);
+list.Add(3);
+list.Add(4);
+list.Add(5);
+Console.WriteLine(list.Find(3));
+foreach (var item in list)
+{
+    Console.WriteLine(item);
+}
